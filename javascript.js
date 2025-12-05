73c955dc93ba35078032f1d7bd58fe1c0c1c6cb4
Jackson <jacksonbryan280@gmail.com>
2025-12-05 14:17:48 -0500

    Created a IIFE gameboard thata returns the board

--- javascript.js
+++ javascript.js
@@ -1,8 +1,27 @@
-const count = document.querySelector("#count");
-const countBtn = document.querySelector("#countBtn");
-let clickCount = 0;
+// const count = document.querySelector("#count");
+// const countBtn = document.querySelector("#countBtn");
+// let clickCount = 0;
 
-countBtn.addEventListener("click", () => {
-  clickCount = clickCount + 1;
-  count.textContent = clickCount;
-});
+// countBtn.addEventListener("click", () => {
+//   clickCount = clickCount + 1;
+//   count.textContent = clickCount;
+// });
+
+//Created gameboard as a IIFE function, so only this one exists
+const Gameboard = (function () {
+  const rows = 6;
+  const columns = 7;
+  const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  //Lets us get the currrent status of the board
+  const getBoard = () => board;
+
+  //allowing us to access the following fuctions
+  return { getBoard };
+})();
+
+console.log(Gameboard.getBoard());
