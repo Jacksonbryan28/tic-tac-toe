f7e16651ace744d4eae6330478c625d59cd4494d
Jackson <jacksonbryan280@gmail.com>
2025-12-06 23:01:49 -0500

    Added ability to get current player, and swap player turn in gameControl

--- javascript.js
+++ javascript.js
@@ -30,12 +30,39 @@ function GameControl(playerOneName, playerTwoName) {
       token: "O",
     },
   ];
-  return players;
+  let currentPlayer = players[0];
+
+  const getCurrentPlayer = () => currentPlayer;
+  const switchPlayerTurn = () => {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+  };
+
+  return { players, getCurrentPlayer, switchPlayerTurn };
 }
 
-let gameOne = GameControl("Jackson", "Darcy");
-console.log(gameOne[1].name);
+// const GameControl = (function (playerOneName, playerTwoName) {
+//   const players = [
+//     {
+//       name: playerOneName,
+//       token: "X",
+//     },
+//     {
+//       name: playerTwoName,
+//       token: "O",
+//     },
+//   ];
+//   return { players };
+// })("Jackson", "Darcy");
+
+const gameController = GameControl("Jackson", "Darcy");
+console.log(gameController.players[0].name);
+// console.log(GameControl.players[0].name);
 console.log(Gameboard.getBoard());
 Gameboard.placeToken("X", 0, 0);
 Gameboard.placeToken("0", 2, 1);
 Gameboard.placeToken("X", 0, 1);
+console.log(gameController.getCurrentPlayer());
+gameController.switchPlayerTurn();
+console.log(gameController.getCurrentPlayer());
+gameController.switchPlayerTurn();
+console.log(gameController.getCurrentPlayer());
